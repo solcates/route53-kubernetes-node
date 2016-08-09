e336ed12db83503bb6382ba3bccd8b664c59e01f
Sol Cates <solcates@gmail.com>
2016-08-09 04:48:32 -0700

    added base64 encoded variable support

--- index.js
+++ index.js
@@ -3,18 +3,43 @@ var fs = require("fs"),
     AWS = require("aws-sdk")
 
 var kubernetesService = process.env["KUBERNETES_SERVICE_HOST"];
+// PATHS
 var kubernetesCAFile = process.env["CA_FILE_PATH"] || "./ca.pem";
 var kubernetesClientCert = process.env["CERT_FILE_PATH"] || "./cert.pem";
 var kubernetesClientKey = process.env["KEY_FILE_PATH"] || "./key.pem";
 var awsCredentials = process.env["AWS_SHARED_CREDENTIALS_FILE"] || "./credentials";
+
+// Base64 Environment Variables
+var kubernetesCAData = process.env["CA_FILE_DATA"];
+var kubernetesClientCertData = process.env['CERT_FILE_DATA'];
+var kubernetesClientKeyData = process.env['KEY_FILE_DATA'];
+var awsCredentialsData = process.env["AWS_SHARED_CREDENTIALS_DATA"];
+
+if (kubernetesCAData) {
+    fs.writeFile("./ca.pem", new Buffer(kubernetesCAData, 'base64').toString("ascii"))
+    kubernetesCAFile = "./ca.pem";
+}
+if (kubernetesClientCertData) {
+    fs.writeFile("./cert.pem", new Buffer(kubernetesClientCertData, 'base64').toString("ascii"));
+    kubernetesClientCert = "./cert.pem";
+}
+if (kubernetesClientKeyData){
+    fs.writeFile("./key.pem", new Buffer(kubernetesClientKeyData, 'base64').toString("ascii"))
+    kubernetesClientKey = "./key.pem"
+}
+if (awsCredentialsData){
+    fs.writeFile("./credentials", new Buffer(awsCredentialsData, 'base64').toString("ascii"));
+    awsCredentials = "./credentials"
+}
+
 var awsRegion = process.env["AWS_DEFAULT_REGION"] || "us-west-2"
 var loopInterval = process.env["LOOP_INTERVAL"] || "5000"
 
+
 var timeout = parseInt(loopInterval);
 
-// var awsCredentials = process.env[""]
 
-
+// Setup K8S client
 var client = new Client({
     host: kubernetesService,
     protocol: 'https',
@@ -25,13 +50,15 @@ var client = new Client({
 
 });
 
-
+// Setup credentials for our AWS clients... ELB and Route53
 var credentials = new AWS.SharedIniFileCredentials({filename: awsCredentials, profile: "default"});
 AWS.config.credentials = credentials;
 AWS.config.region = awsRegion;
 var awsELBClient = new AWS.ELB();
 var awsR53Client = new AWS.Route53();
 
+
+// Update DNS records (CREATE and UPSERT)
 var updateDNS = function (action, zone_id, domainName, elb_address) {
     var params = {
         "HostedZoneId": zone_id, // our Id from the first call
@@ -54,17 +81,13 @@ var updateDNS = function (action, zone_id, domainName, elb_address) {
         }
     };
     // console.log(params)
-    awsR53Client.changeResourceRecordSets(params, function(err, result){
-        if (err){
+    awsR53Client.changeResourceRecordSets(params, function (err, result) {
+        if (err) {
             console.error(err)
         } else {
             // Successfully updated your record... not much to do now :)
         }
     })
-}
-
-var checkDNS = function (domainName) {
-
 }
 
 
@@ -106,24 +129,21 @@ var workLoop = function (done) {
                                                                     if (record.Name == (domainName + ".")) {
                                                                         updated = true;
                                                                         var rr = record.ResourceRecords[0]
-                                                                        if (rr.Value == lb.DNSName){
+                                                                        if (rr.Value == lb.DNSName) {
                                                                             console.log("Recordset already set for:", domainName)
                                                                         } else {
-                                                                            console.log("Changing ResourceSet:", domainName, "=>" , lb.DNSName );
+                                                                            console.log("Changing ResourceSet:", domainName, "=>", lb.DNSName);
 
-                                                                            updateDNS("UPSERT",zone.Id,domainName,lb.DNSName)
+                                                                            updateDNS("UPSERT", zone.Id, domainName, lb.DNSName)
                                                                         }
 
                                                                     }
                                                                 })
-                                                                if (!updated){
-                                                                    console.log("Inserting ResourceSet:", domainName, "=>" , lb.DNSName );
-                                                                    updateDNS("CREATE",zone.Id,domainName,lb.DNSName)
+                                                                if (!updated) {
+                                                                    console.log("Inserting ResourceSet:", domainName, "=>", lb.DNSName);
+                                                                    updateDNS("CREATE", zone.Id, domainName, lb.DNSName)
                                                                 }
                                                             })
-
-                                                            // console.log("HostedZone:", zone)
-                                                            // console.log("DomainName: ", domainName)
                                                         }
                                                     })
                                                 }
